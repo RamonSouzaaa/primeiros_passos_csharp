226986b744111b4224d056fe5d473d9aae2b4c6d
RamonSouzaaa <ramonsouza859@gmail.com>
2024-03-09 17:22:24 -0300

    Projeto balta.io cronônometro

---
+++ Cronometro/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace Cronometro
+{
+    class Program
+    {
+        public static void Start()
+        {
+            short h = 0, m = 0, s = 0;
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    Console.Clear();
+                    s++;
+
+                    if (s == 60)
+                    {
+                        s = 0;
+                        m++;
+                    }
+
+                    if (m == 60)
+                    {
+                        m = 0;
+                        h++;
+                    }
+
+                    if (h == 24)
+                    {
+                        h = 0;
+                    }
+
+                    Console.WriteLine($"{h.ToString("00")}:{m.ToString("00")}:{s.ToString("00")}");
+                    Console.WriteLine("Press enter key to stop");
+                    Thread.Sleep(1000);
+                }
+            } while (Console.ReadKey().Key != ConsoleKey.Enter);
+        }
+
+        static void Main(string[] args)
+        {
+            Start();
+        }
+    }
+}
